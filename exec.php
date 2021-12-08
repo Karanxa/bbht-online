230814b10932a6937f3bc5534f38136ae1c0b099
Danish Shakeel <danishshakeel54@gmail.com>
2021-12-09 01:23:35 +0530

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,10 +1,16 @@
 <?php
-
+// $DIR_PATH = "";
 function exec_command() {
-            if ($_POST['command']){
-                $program=$_POST['command'];
-                $output = shell_exec('/var/www/html/recon.sh $program');
-                echo $output;
-        }
+	// $var_exists = shell_exec( "echo \$RECON_PATH" );
+	
+	// if ( empty( $var_exists ) ) {
+	// 	shell_exec( "export RECON_PATH=" . $DIR_PATH );
+	// }
+
+	if ( isset( $_POST['command'] ) ){
+			$program=$_POST['command'];
+			$output = shell_exec('bash /var/www/html/bbht-online/recon.sh' . ' ' . $program);
+			echo $output;
+	}
 }
 exec_command();
